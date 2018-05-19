144595c099a5d694a4483f3fdba1fbd59594ec85
Гурьев Василий Александрович <Vaschudo@gmail.com>
2018-05-19 22:12:59 +0300

    Submit test of lan and lan program.

---
+++ LAN/LAN.fs
@@ -0,0 +1,62 @@
+﻿open System
+
+type System = Windows = 0 | Linux = 1 | MacOS = 2
+
+type OS() =
+    let mutable pW = 0.0 
+    let mutable  pL = 0.0 
+    let mutable  pMOS = 0.0 
+    
+    member this.setProbInfect pForWindows pForLinux pForMacOS =
+        pW <- pForWindows
+        pL <- pForLinux
+        pMOS <- pForMacOS
+
+    member this.getProbOfInfect(os : System) = 
+        match os with
+        | System.Windows -> pW
+        | System.Linux -> pL
+        | System.MacOS -> pMOS
+    
+type Comp(oSystem : System, isInfected : bool) =
+    member val os = oSystem with get, set
+    member val infect = isInfected with get, set
+    member val isInfectedToday = true with get, set
+
+type Lan(newComputers : array<Comp>, newAdMatrix : bool[,], newOS : OS) =
+    let mutable computers = newComputers
+    let mutable adMatrix = newAdMatrix
+    let rand = new Random()
+    let os = newOS
+    do
+        if (computers.Length <> Array2D.length1 adMatrix || computers.Length <> Array2D.length2 adMatrix)
+        then 
+            raise (ArgumentException("Lenght of computers array disagree with length of matrix!"))
+
+    let isInfectedNow numOfComp =
+        rand.Next(100) <= int (100.0 * os.getProbOfInfect(computers.[numOfComp].os))
+
+    let infectAllNeighbor numOfComp =
+        for numOfNeigh in 0..computers.Length - 1 do        
+            if (adMatrix.[numOfComp, numOfNeigh] = true && not computers.[numOfNeigh].infect && (isInfectedNow numOfNeigh))
+            then
+                computers.[numOfNeigh].infect <- true
+                computers.[numOfNeigh].isInfectedToday <- true
+
+    let nextStep() =
+        for i in 0..computers.Length - 1 do
+            if (computers.[i].infect = true && computers.[i].isInfectedToday = false)
+            then
+                infectAllNeighbor i 
+
+    let nextStepBegin() =
+        for comp in computers do
+            comp.isInfectedToday <- false
+
+    member this.NextStep() =
+        nextStepBegin() 
+        nextStep()
+    
+    member this.Computers
+        with get() =
+            computers
---
+++ TestLAN/TestLANMain.fs
@@ -0,0 +1,49 @@
+﻿open NUnit.Framework
+open FsUnit
+open System
+open LAN
+
+[<Test>]
+let ``test LAN 1`` () =
+    let os = new OS()
+    os.setProbInfect 1.0 0.0 0.0
+    let lan = new Lan([|new Comp(System.Windows, false)|], Array2D.create 1 1 true, os)
+    lan.NextStep()
+    (lan.Computers.Length = 1 && lan.Computers.[0].infect = false) |> should be True
+
+let ``test LAN 2`` () =
+    let os = new OS()
+    os.setProbInfect 1.0 0.0 1.0
+    let lan = new Lan([|new Comp(System.MacOS, true); new Comp(System.Linux, false)|], Array2D.create 2 2 false, os)
+    for i in 0..100 do
+        lan.NextStep()
+    let comp = lan.Computers
+    (comp.Length = 2 && comp.[0].infect = true && comp.[1].infect = false) |> should be True
+
+let ``test LAN 3`` () =
+    let os = new OS()
+    os.setProbInfect 1.0 0.0 1.0
+    fun() -> new Lan([|new Comp(System.MacOS, true); new Comp(System.Linux, false)|], Array2D.create 2 3 false, os) |> ignore |> should throw typeof<ArgumentException>  
+
+let ``test LAN 4`` () =
+    let os = new OS()
+    os.setProbInfect 1.0 1.0 0.5
+    
+    let mutable matrix = Array2D.create 3 3 true
+    matrix.[0, 1] <- false
+    matrix.[0, 2] <- false
+    matrix.[1, 0] <- false
+    matrix.[2, 0] <- false
+
+    let lan = new Lan([|new Comp(System.Windows, false); new Comp(System.Linux, false); new Comp(System.MacOS, true)|], matrix, os)
+    lan.NextStep()
+    let comp = lan.Computers
+    (lan.Computers.[0].infect = false && comp.[1].infect = true) |> should be True
+
+[<EntryPoint>]
+let main argv =
+    ``test LAN 1`` ()
+    ``test LAN 2`` ()
+    ``test LAN 3`` ()
+    ``test LAN 4`` ()
+    0
